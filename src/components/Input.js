df3bdce6146463f489aeed8b7469b7d9c3deaa88
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-07-31 22:30:35 +0300

    Input: Step 1 is completed: State hook pair is created and inputDegeri is initialized with an empty string

--- src/components/Input.js
+++ src/components/Input.js
@@ -37,6 +37,7 @@ import React, { useState } from "react"; /* ADIM 0 */
 
 export default function Input() {
   /* ADIM 1 */
+  const [inputDegeri, setInputDegeri] = useState("");
 
   const inputuDeğiştir = (evt) => {
     // When the input changes, its whole value can be found inside the event object.
